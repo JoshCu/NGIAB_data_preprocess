08907753f4749d6d8c24174b3ad05f781b926694
JoshCu <josh.cu@gmail.com>
2024-01-16 20:22:09 -0600

    determine vpu bounds from tiles

--- map_app/views.py
+++ map_app/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify
 import geopandas as gpd
 import requests
+import re
+import math
 
 main = Blueprint('main', __name__)
 
@@ -25,3 +27,38 @@ def handle_map_interaction():
 
     # Return a JSON response
     return jsonify(result)
+
+def convert_grid_to_coords(xmin, ymin, xmax, ymax):
+    # accept a list of grid coordinates and return a list of lat/lon coordinates
+    zoom = 18
+    n = 2 ** zoom
+    xmin = xmin / n * 360.0 - 180.0
+    xmax = xmax / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ymin / n)))
+    ymin = lat_rad * 180.0 / math.pi
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ymax / n)))
+    ymax = lat_rad * 180.0 / math.pi
+    return [xmin, abs(ymin), xmax, abs(ymax)]
+
+@main.route('/get_map_data', methods=['GET'])
+def get_map_data():
+    # Get the URL from the query string
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "Missing URL parameter"}), 400
+    try:
+        # Make the request to the external URL
+        response = requests.get(url)
+        if response.status_code == 404:
+            # text.match(/([0-9]+,){0}/)[0].slice(0,-1);
+            results = re.findall(r'[0-9]+(?=,)', response.content.decode('utf-8'))
+            results = results[:4]
+            # cast to float
+            results = [float(x) for x in results]
+            bounds = convert_grid_to_coords(*results)
+            return bounds, 200
+        # Return the content of the response
+        return response.content, response.status_code
+    except requests.RequestException as e:
+        # Handle any errors that occur during the request
+        return jsonify({"error": str(e)}), 500
