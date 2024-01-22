dcf9b8e35e9e98435054c44a0676c4991ace591f
Josh <josh.cu@gmail.com>
2024-01-22 17:52:14 -0600

    update forcings path to correct place inside ./outputs

--- exact_forcings.py
+++ exact_forcings.py
@@ -39,7 +39,7 @@ def compute_store(stores):
     return merged_data
 
 
-def compute_and_save(rasters, gdf, time, variable):
+def compute_and_save(rasters, gdf, time, forcings_dir, variable):
     raster=rasters[variable]
     #print(f'Computing {variable} for {time}')
     results = exact_extract(raster, gdf, ['mean'], include_cols=['divide_id'], output='pandas')
@@ -52,73 +52,85 @@ def compute_and_save(rasters, gdf, time, variable):
         # should use RAINRATE from previous timestep,         chose this   VVVVV   density factor  at random
         results['APCP_surface'] = (results['RAINRATE'] * 3600 * 1000) / 0.998
         
-    results.to_csv(f'temp/star_map/{variable}_{time}.csv')
+    results.to_csv(forcings_dir / f'temp/{variable}_{time}.csv')
     return results
 
-def split_csv_file(file_to_split, timestep):
+def split_csv_file(file_to_split, timestep, forcings_dir):
     datestring = datetime.strptime(timestep.split('/')[-1].split('.')[0], '%Y%m%d%H%M')
-    output_directory = 'temp/by_catchment/'
-    command = f"""awk -F, 'NR > 1 {{output="{datestring}"; for(i=2; i<=NF; i++) output = output ", " $i; print output >> "{output_directory}"$1".csv"}}' {file_to_split}"""
+    output_directory = forcings_dir / 'by_catchment'
+    command = f"""awk -F, 'NR > 1 {{output="{datestring}"; for(i=2; i<=NF; i++) output = output ", " $i; print output >> "{output_directory}/"$1".csv"}}' {file_to_split}"""
     os.system(f'{command}')
 
-def compute_zonal_stats(gdf, merged_data):
+def compute_zonal_stats(gdf, merged_data, forcings_dir):
     # desired output format
     # one file per catchment
     # csv with columns: time, LWDOWN, PSFC, Q2D, RAINRATE, SWDOWN, T2D, U2D, V2D
     # exact extract works best with one timestep, lots of polygons
     print('Computing zonal stats')
-    print(merged_data)
-    all_dfs = []
+    # clear by_time files
+    for file in os.listdir(forcings_dir / 'by_time'):
+        os.remove(forcings_dir / 'by_time' / file)
     with multiprocessing.Pool() as pool:
         for time in merged_data.time.values:
             raster = merged_data.sel(time=time)
-            timestep_result = pool.map(partial(compute_and_save,raster, gdf, time), ['LWDOWN', 'PSFC','Q2D', 'RAINRATE', 'SWDOWN', 'T2D', 'U2D', 'V2D'])
+            timestep_result = pool.map(partial(compute_and_save,raster, gdf, time, forcings_dir), ['LWDOWN', 'PSFC','Q2D', 'RAINRATE', 'SWDOWN', 'T2D', 'U2D', 'V2D'])
             timestep_result = pd.concat(timestep_result, axis=1)
             timestring = datetime.strftime(pd.to_datetime(str(time)), '%Y%m%d%H%M')
-            timestep_result.to_csv(f'temp/by_time/{timestring}.csv')
+            timestep_result.to_csv(forcings_dir / f'by_time/{timestring}.csv')
 
     catchment_ids = gdf['divide_id'].unique()
 
     # clear catchment files
-    for file in os.listdir('temp/by_catchment'):
-        os.remove('temp/by_catchment/' + file)
+    for file in os.listdir(forcings_dir / 'by_catchment'):
+        os.remove(forcings_dir / 'by_catchment' / file)
 
     for cat in catchment_ids:
-        with open(f'temp/by_catchment/{cat}.csv', 'w') as f:
+        with open(forcings_dir / f'by_catchment/{cat}.csv', 'w') as f:
             # do some renaming here 
             #   divide_id, LWDOWN,        PSFC,         Q2D,                RAINRATE,    APCP_surface, SWDOWN,        T2D,               U2D,                 V2D
             f.write('time, DLWRF_surface, PRES_surface, SPFH_2maboveground, precip_rate, APCP_surface, DSWRF_surface, TMP_2maboveground, UGRD_10maboveground, VGRD_10maboveground\n')
 
     # use awk to pivot the csvs to be one per catchment
-    for timestep_file in sorted(os.listdir('temp/by_time')):
-        split_csv_file('temp/by_time/' + timestep_file, timestep_file)
+    for timestep_file in sorted(os.listdir(forcings_dir / 'by_time')):
+        split_csv_file(forcings_dir / 'by_time' / timestep_file, timestep_file, forcings_dir)
 
     # remove files in the start_map directory
-    for file in os.listdir('temp/star_map'):
-        os.remove('temp/star_map/' + file)
+    for file in os.listdir(forcings_dir / 'temp'):
+        os.remove(forcings_dir / 'temp' / file)
+
+def setup_directories(wb_id):
+    data_dir = Path(__file__).parent / 'output' / wb_id / 'config'
+    forcings_dir = Path(__file__).parent / 'output' / wb_id / 'forcings'
+    geopackage_path = data_dir / f'{wb_id}_subset.gpkg'
+    template_file = Path(__file__).parent / 'data_sources' / 'template.nc'
+
+    for folder in ['by_time', 'by_catchment', 'temp']:
+        os.makedirs(forcings_dir / folder, exist_ok=True)
+    return forcings_dir, geopackage_path, template_file
+
 
 def create_forcings(start_time, end_time, wb_id):
-    data_directory = Path(__file__).parent / 'output' / wb_id / 'config'
-    geopackage_path = data_directory / f'{wb_id}_subset.gpkg'
-    template_file = Path(__file__).parent / 'data_sources' / 'template.nc'
-    
-    #lazy_store = get_zarr_stores(start_time, end_time)
-    print('Got zarr stores')
+
+    forcings_dir, geopackage_path, template_file = setup_directories(wb_id)
     projection = xr.open_dataset(template_file,engine='netcdf4').crs.esri_pe_string
-    print('Got grid file')
+    print('Got projection from grid file')
     gdf = get_gdf(geopackage_path, projection)
     print('Got gdf')
-    #clipped_store = clip_stores_to_catchments(lazy_store, gdf.total_bounds)
-    print('Clipped stores')
-    #merged_data = compute_store(clipped_store)
-    print('Computed store')
-    merged_data = xr.open_dataset('merged_data.nc')
-    compute_zonal_stats(gdf, merged_data)
+    
+    if not os.path.exists(forcings_dir / 'merged_data.nc'):
+        #lazy_store = get_zarr_stores(start_time, end_time)
+        print('Got zarr stores')
+        #clipped_store = clip_stores_to_catchments(lazy_store, gdf.total_bounds)
+        print('Clipped stores')
+        #merged_data = compute_store(clipped_store)
+        print('Computed store')
+    merged_data = xr.open_dataset(forcings_dir / 'merged_data.nc')
+    compute_zonal_stats(gdf, merged_data, forcings_dir)
     
 
 if __name__ == '__main__':
     start_time = '2010-01-01 00:00'
-    end_time = '2010-01-10 00:00'
+    end_time = '2010-01-02 00:00'
     wb_id = 'wb-1643991'
     create_forcings(start_time, end_time, wb_id)
     #takes 9m21s on i9-10850k 20 cores
