78a3cc8d182a65adf7a2ef9549f0879225e2d3ed
Josh <josh.cu@gmail.com>
2024-02-28 15:40:11 -0600

    only run debug with .dev

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-flask -A map_app run --debug
+
+# clear the app log
+# the file needs to exist and have non whitespace content
+echo "Starting Application!" > app.log
+
+
+# if .dev is present run in debug mode
+if [ -f .dev ]; then
+    echo "Running in debug mode"
+    flask -A map_app run --debug
+else
+    echo "Running in production mode"
+    flask -A map_app run
+fi
